34251f80841d2bed336f5485e5582d1beb60643b
Zim <73538267+Mohamm4@users.noreply.github.com>
2020-10-28 14:56:01 +0600

    z1m.sh

---
+++ z1m.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+#This tool is created by zim
+
+
+#target
+echo 'are you want to continue'
+read target
+apt update 
+apt upgrade
+apt install git
+apt install python
+apt install sl
+
+echo '____AUTHOR___Mohammad zim'
+echo 'Danish sheikh is my friend'
+echo 'press enter'
+read name
+clear
+
+git clone https://github. com/pashayogi/SETAN-GOLD
+ls
+echo 'now type ls and one the py file'
+read name 
+clear
